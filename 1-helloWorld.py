9388db5bf3c6a91702fd3d2a6f9ba950eeeb1d2c
Carlos Fuentes <educharlief@hotmail.com>
2016-09-19 17:05:44 -0500

    My first hello world with TensorFlow

---
+++ 1-helloWorld.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+x = tf.constant(1,name='x')
+y = tf.Variable(x+9,name='y')
+
+model = tf.initialize_all_variables()
+
+with tf.Session() as session:
+	session.run(model)
+	print(session.run(y))
