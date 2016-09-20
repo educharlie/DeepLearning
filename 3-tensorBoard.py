3bda04f85605827a55e4dbbb06e9bc4abab0f87f
Carlos Fuentes <educharlief@hotmail.com>
2016-09-19 19:47:59 -0500

    Add a simple integer mutiplication and visualize the graph flow in TensorBoard

---
+++ 3-tensorBoard.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+a = tf.constant(10, name="a")
+b = tf.constant(90, name="b")
+y = tf.Variable(a+b*2, name="y")
+
+model = tf.initialize_all_variables()
+
+with tf.Session() as session:
+	merged = tf.merge_all_summaries()
+	writer = tf.train.SummaryWriter\
+						("/tmp/tensorflowlogs", session.graph)
+	session.run(model)
+	print(session.run(y))
+
+#tensorboard --logdir=/tmp/tensorflowlogs
